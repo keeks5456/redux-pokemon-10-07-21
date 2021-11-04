4472c051df78f601c64d4e913ce829b776853db8
Akiko Green <agreen@alphaworks.tech>
2021-11-04 14:30:00 -0700

    setting up reducer

--- src/redux/reducers/pokemonReducers.js
+++ src/redux/reducers/pokemonReducers.js
@@ -1,6 +1,29 @@
 const initialState = {
   loading: false,
-  pokeDEx: [],
+  pokeDex: [],
   pokeTeam: [],
-  error: ""
-}
+  error: "",
+};
+
+const pokemonReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "FETCH_POKEMON_REQUEST":
+      return {
+        loading: true,
+      };
+    case "FETCH_POKEMON_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        pokeDex: action.payload,
+      };
+    case "FETCH_POKEMON_FALIURE":
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
